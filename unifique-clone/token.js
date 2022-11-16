31fc7df5c5a955d00e42147f789cc156cafc200d
eduardo.correa1 <eduardo.correa1@alunos.sc.senac.br>
2022-11-16 16:01:05 -0300

    ok

---
+++ unifique-clone/token.js
@@ -0,0 +1,7 @@
+function getUrlBase(port) {
+    return "https://lniahqtiviipzdhghugw.supabase.co" + port + "select=*";
+}
+
+function getTokenSupabase() {
+    return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxuaWFocXRpdmlpcHpkaGdodWd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMwMDE0MzgsImV4cCI6MTk3ODU3NzQzOH0.W1Q2aKD2Nnh9Fing3yCJ4hplIaH45m0gG62TV4zGljc";
+}
